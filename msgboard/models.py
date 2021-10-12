1ca587948f34a7a816604143b59c50da36f34bf5
Arad Zilberstein <arad1997@gmail.com>
2021-10-12 10:56:22 +0300

    Last one

--- msgboard/models.py
+++ msgboard/models.py
@@ -1,11 +1,8 @@
-
-
-# Create your models here.
-
 from django.db import models
 from django.utils import timezone
 
+
 class Message(models.Model):
-	author = models.CharField(max_length=200)
-	text = models.TextField()
-	date = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
